100f991cd3f2e820ab3f11c060648beac20d7810
Vyshakh Suresh <sureshvyshakh01@gmail.com>
2026-07-06 22:28:30 +0530

    base_model.py added_01

---
+++ Backend/app/Models/base_model.py
@@ -0,0 +1,18 @@
+from app.database.base import Base
+from datetime import datetime
+from sqlalchemy import DateTime,Boolean
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy.sql import func
+
+
+class BaseModel(Base):
+    # Use abstract base model for not creating the table for this class bacause of this model act as parent for all other classes.
+    
+    __abstract__ =True
+    
+    id: Mapped[int] = mapped_column(primary_key=True,index=True)
+    
+    created_at: Mapped[datetime] =mapped_column(DateTime(timezone=True),
+                                                server_default=func.now(),
+                                                nullable=False)
+    
