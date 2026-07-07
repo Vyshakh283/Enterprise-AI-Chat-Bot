ce12dc82b0c2d5e056ba9ab1475fbedc333e56f5
Vyshakh Suresh <sureshvyshakh01@gmail.com>
2026-07-07 21:37:33 +0530

    base.py updated

--- Backend/app/Models/base_model.py
+++ Backend/app/Models/base_model.py
@@ -15,4 +15,5 @@ class BaseModel(Base):
     created_at: Mapped[datetime] =mapped_column(DateTime(timezone=True),
                                                 server_default=func.now(),
                                                 nullable=False)
-    
+    updated_at: Mapped[datetime] =mapped_column(DateTime(timezone=True),onupdate=func.now(),
+                                                nullable=False)
